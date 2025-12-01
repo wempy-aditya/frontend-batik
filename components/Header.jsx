a91f2431cc5df6c1cf115dd1123d4a794eb8946a
Wempy Aditya <adityawempy@gmail.com>
2025-12-01 12:22:48 +0700

    merge to main

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,13 +25,6 @@ const Header = () => {
     { name: "Features", path: "/features" },
     { name: "About", path: "/about" },
     { name: "Contact", path: "/contact" },
-    { name: "Home", path: "/" },
-    { name: "Projects", path: "/projects" },
-    { name: "Datasets", path: "/datasets" },
-    { name: "Publications", path: "/publications" },
-    { name: "Features", path: "/features" },
-    { name: "Creator Studio", path: "/test-api" },
-    { name: "Advanced Studio", path: "/advanced-batik" },
   ];
 
   const isActive = (path) => pathname === path;
