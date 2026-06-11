b4272b81ad0f5dfa5e826ca48da34c75ecf021f3
Wempy Aditya <adityawempy@gmail.com>
2026-06-11 15:39:20 +0700

    update demo preview page

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -607,7 +607,7 @@ export default function ProjectDetailPage() {
                     <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
                       <path d="M8 5v14l11-7z" />
                     </svg>
-                    Buka Preview
+                    Demo Preview
                   </button>
                 )}
                 {hasDemos && (
